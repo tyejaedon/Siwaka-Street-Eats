ac77ff819f5c6b49c1314f0c2582d2c13d40460b
tyejaedon <82274559+tyejaedon@users.noreply.github.com>
2024-05-29 15:54:04 +0300

    The addition of a hover feature on the login div

---
+++ index.js
@@ -0,0 +1,9 @@
+const container = document.querySelectorAll('.wrapper');
+
+container.addEventListener('mouseover', function() {
+    this.classList.add('centered');
+});
+
+container.addEventListener('mouseout', function() {
+    this.classList.remove('centered');
+});
